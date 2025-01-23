12684cbfe68c92e7e0b14f867dd577d5a66afbe0
PaulG09 <lotuds107@gmail.com>
2025-01-23 16:02:04 +0000

    learn page structure created

--- frontend/learn/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Ensure "Education" link is active and not clickable
-  const educationLink = document.querySelector(".nav-item .nav-link.selected");
-
-  if (educationLink) {
-    educationLink.classList.add("active");
-    educationLink.style.pointerEvents = "none"; // Disable click
-  }
-});
